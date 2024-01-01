9a437ca92172f9e3e776da0a7ced7ac054e0219a
shafikul-islam <179398ssshofik@gmail.com>
2024-01-01 12:27:22 +0600

    [DONE] retrive and show UI the recent message with contacts

--- src/components/Chatlist/ChatLIstItem.jsx
+++ src/components/Chatlist/ChatLIstItem.jsx
@@ -2,16 +2,34 @@ import React from "react";
 import Avatar from "../common/Avatar";
 import { useStateProvider } from "@/context/StateContext";
 import { actionCases } from "@/context/constants";
+import { calculateTime } from "@/utils/CalculateTime";
+import MessageStatus from "../common/MessageStatus";
+import { FaCamera, FaMicrophone } from "react-icons/fa";
 
 function ChatLIstItem({ data, isContactPage = false }) {
   const [{ userInfo, currentChatUser }, dispatch] = useStateProvider();
-  const handleContactClick = () => {
-    dispatch({
-      type: actionCases.CHANGE_CURRENT_CHAT_USER,
-      user: { ...data },
-    });
 
-    dispatch({ type: actionCases.SET_ALL_CONTACT_PAGE });
+  const handleContactClick = () => {
+    if (!isContactPage) {
+      dispatch({
+        type: actionCases.CHANGE_CURRENT_CHAT_USER,
+        user: {
+          name: data.name,
+          about: data.about,
+          email: data.email,
+          profilePhoto: data.profilePhoto,
+          id:
+            userInfo.id === data?.senderId ? data?.receiverId : data?.senderId,
+        },
+      });
+    } else {
+      dispatch({
+        type: actionCases.CHANGE_CURRENT_CHAT_USER,
+        user: { ...data },
+      });
+
+      dispatch({ type: actionCases.SET_ALL_CONTACT_PAGE });
+    }
   };
 
   return (
@@ -28,12 +46,56 @@ function ChatLIstItem({ data, isContactPage = false }) {
           <div>
             <span className="text-white">{data?.name}</span>
           </div>
+          {!isContactPage && (
+            <div>
+              <span
+                className={`${
+                  data?.totalUnreadMessages > 0
+                    ? "text-icon-green"
+                    : "text-secondary"
+                } text-sm`}
+              >
+                {calculateTime(data?.createdAt)}
+              </span>
+            </div>
+          )}
         </div>
         <div className="flex pt-1 pb-2 border-b pe-3 border-conversation-border">
           <div className="flex justify-between w-full">
             <span className="text-sm text-secondary line-clamp-1">
-              {data?.about || "\u00A0"}
+              {isContactPage ? (
+                data?.about || "\u00A0"
+              ) : (
+                <div className="flex items-center  gap-1 max-w-[200px] sm:max-w-[250px] md:max-w-[300px]  lg:max-w-[200px] xl:max-w-[300px] ">
+                  {data?.senderId === userInfo?.id && (
+                    <MessageStatus messageStatus={data?.messageStatus} />
+                  )}
+
+                  {data?.type === "text" && (
+                    <span className="truncate">{data?.message}</span>
+                  )}
+
+                  {data?.type === "image" && (
+                    <div className="flex items-center gap-1">
+                      <FaCamera className="text-panel-header-icon" />
+                      Image
+                    </div>
+                  )}
+
+                  {data?.type === "audio" && (
+                    <div className="flex items-center gap-1">
+                      <FaMicrophone className="text-panel-header-icon" />
+                      Audio
+                    </div>
+                  )}
+                </div>
+              )}
             </span>
+            {data?.totalUnreadMessages > 0 && (
+              <span className="bg-icon-green px-12 rounded-full text-sm">
+                {data?.totalUnreadMessages}
+              </span>
+            )}
           </div>
         </div>
       </div>
