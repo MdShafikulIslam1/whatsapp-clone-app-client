0046b36e87f1298b2115862ad165f3227a4c524b
shafikul-islam <179398ssshofik@gmail.com>
2024-01-01 20:59:10 +0600

    [DONE] voice call show UI with controled accept and reject button also intregate this socket.io

--- src/components/common/IncomingCall.jsx
+++ src/components/common/IncomingCall.jsx
@@ -1,46 +1,59 @@
 import { useStateProvider } from "@/context/StateContext";
+import { actionCases } from "@/context/constants";
 import Image from "next/image";
 import React from "react";
 
 function IncomingCall() {
-  // const [{ incomingVideoCall }, dispatch] = useStateProvider();
+  const [{ incomingVoiceCall, socket }, dispatch] = useStateProvider();
 
-  // const acceptCall = () => {};
+  const acceptCall = () => {
+    dispatch({
+      type: actionCases.SET_VOICE_CALL,
+      voiceCall: { ...incomingVoiceCall, type: "in-coming" },
+    });
 
-  // const rejectCall = () => {};
+    socket.current.emit("accept-incoming-call", { id: incomingVoiceCall?.id });
+    dispatch({
+      type: actionCases.SET_INCOMING_VOICE_CALL,
+      incomingVoiceCall: undefined,
+    });
+  };
 
-  // return (
-  //   <div className="fixed z-50 flex items-center justify-start h-16 gap-5 p-4 mb-0 text-white border-2 rounded-sm bottom-8 right-6 w-80 bg-conversation-panel-background drop-shadow-2xl border-icon-green py-14 ">
-  //     <div>
-  //       <Image
-  //         src={incomingVideoCall?.profilePhoto}
-  //         alt="profile-image"
-  //         width={70}
-  //         height={70}
-  //       />
-  //     </div>
-  //     <div>
-  //       <div>{incomingVideoCall?.name}</div>
-  //       <div className="text-xm">Income Video call</div>
-  //       <div className="flex gap-2 mt-2">
-  //         <button
-  //           className="p-1 px-3 text-sm bg-red-500 rounded-full"
-  //           onClick={rejectCall}
-  //         >
-  //           Reject
-  //         </button>
-  //         <button
-  //           className="p-1 px-3 text-sm rounded-full bg-green-600-500"
-  //           onClick={acceptCall}
-  //         >
-  //           Accept
-  //         </button>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+  const rejectCall = () => {
+    socket.current.emit("reject-voice-call", { from: incomingVoiceCall?.id });
+    dispatch({ type: actionCases.END_CALL });
+  };
 
-  return <div>Incoming voice call</div>;
+  return (
+    <div className="fixed z-50 flex items-center justify-start h-16 gap-5 p-4 mb-0 text-white border-2 rounded-sm bottom-8 right-6 w-80 bg-conversation-panel-background drop-shadow-2xl border-icon-green py-14 ">
+      <div>
+        <Image
+          src={incomingVoiceCall?.profilePhoto || "/avatar/4.png"}
+          alt="profile-image"
+          width={70}
+          height={70}
+        />
+      </div>
+      <div>
+        <div>{incomingVoiceCall?.name}</div>
+        <div className="text-xm">Income Voice call</div>
+        <div className="flex gap-2 mt-2">
+          <button
+            className="p-1 px-3 text-sm bg-red-500 rounded-full"
+            onClick={rejectCall}
+          >
+            Reject
+          </button>
+          <button
+            className="p-1 px-3 text-sm rounded-full bg-green-600-500"
+            onClick={acceptCall}
+          >
+            Accept
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default IncomingCall;
